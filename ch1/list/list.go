73cb6e1e506ec561c853a4fc79176cef095f5d5d
igorakimy <igorakimy@gmail.com>
2023-07-02 20:24:14 +0300

    add ch1 - list

---
+++ ch1/list/list.go
@@ -0,0 +1,23 @@
+package main
+
+// importing fmr and container list packages
+import (
+	"container/list"
+	"fmt"
+)
+
+// main method
+func main() {
+	var intList list.List
+	intList.PushBack(11)
+	intList.PushBack(23)
+	intList.PushBack(34)
+
+	for element := intList.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(int))
+	}
+}
+
+// 11
+// 24
+// 34
