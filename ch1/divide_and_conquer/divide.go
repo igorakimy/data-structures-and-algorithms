eb533649603f3128bc92d951368fe9a96f218456
igorakimy <igorakimy@gmail.com>
2023-07-03 06:58:32 +0300

    add ch1 - divide and conquer

---
+++ ch1/divide_and_conquer/divide.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(k int) int {
+	if k <= 1 {
+		return 1
+	}
+	return fibonacci(k-1) + fibonacci(k-2)
+}
+
+func main() {
+	var m = 5
+	for m = 0; m < 8; m++ {
+		var fib = fibonacci(m)
+		fmt.Println(fib)
+	}
+}
+
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
