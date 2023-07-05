f2aa54a331c2902193d57d69d3d48e565bf03060
igorakimy <igorakimy@gmail.com>
2023-07-05 22:02:24 +0300

    add ch2 - basic slice

---
+++ ch2/basic_slice.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var slice = []int{1, 3, 5, 6}
+	slice = append(slice, 8)
+	fmt.Println("Capacity", cap(slice))
+	fmt.Println("Length", len(slice))
+}
+
+// Capacity 8
+// Length 5
